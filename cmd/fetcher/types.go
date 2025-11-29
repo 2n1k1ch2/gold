dbeda6d1e30e0f4b4aad6dab4db2f42a732a5794
nbaev <nikita_uray@mail.ru>
2025-11-30 00:15:51 +0300

    feat: implement snapshot fetcher

---
+++ cmd/fetcher/types.go
@@ -0,0 +1,16 @@
+package fetcher
+
+type Fetcher interface {
+	Collect() (*RuntimeSnapshot, error)
+}
+
+type RuntimeSnapshot struct {
+	Service   string
+	Timestamp int64
+
+	GoroutineDump  []byte // debug=2
+	BlockProfile   []byte // pprof binary
+	MutexProfile   []byte // pprof binary
+	RuntimeMetrics map[string]float64
+	Version        string
+}
